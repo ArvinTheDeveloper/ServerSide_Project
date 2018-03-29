1ee1cd4de70e8474aee089a53f82712584771cbb
LifeOfArvin <arvinmaryami@gmail.com>
2018-03-29 14:24:49 +0200

    as

--- ServerSide_Project/Controllers/AuthorController.cs
+++ ServerSide_Project/Controllers/AuthorController.cs
@@ -14,18 +14,18 @@ namespace ServerSide_Project.Controllers
             return View();
         }
 
-        /*Search: Returns authors that match the searched string as a partial view*/
+        // Search: Returns authors that match the searched string as a partial view
         public ActionResult arSearchAuthor()
         {
             return PartialView("~/Views/shared/_SearchResultView.cshtml");
         }
-        //AddABook: Returns the add book section where the user will enter info about a new book
+        // arAddAnAuthor: Returns the add author section where the user will enter info about a new author
         public ActionResult arAddAnAuthor()
         {
             return PartialView("~/Views/Author/_AddAuthorView.cshtml");
         }
 
-        //SaveBook: Saves the added book into the database
+        // arSaveAuthor: Saves the added author into the database
         public ActionResult arSaveAuthor()
         {
             return View();
